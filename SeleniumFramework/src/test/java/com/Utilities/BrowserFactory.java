040a8758a65c512bc88fc81cc967ee9f9f53a9f4
shubha95m <46652243+shubha95m@users.noreply.github.com>
2019-11-05 10:54:53 +0530

    Update BrowserFactory.java
    added new method

--- SeleniumFramework/src/test/java/com/Utilities/BrowserFactory.java
+++ SeleniumFramework/src/test/java/com/Utilities/BrowserFactory.java
@@ -55,6 +55,46 @@ public class BrowserFactory {
 		
 	}
 	
+	public static WebDriver startApplication (WebDriver driver, String Browser) {
+		
+		if (Browser.equals("Chrome")) {
+			
+			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+			
+			driver=new ChromeDriver();
+			
+		}
+		
+		else if(Browser.equals("FireFox")) {
+			
+			System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
+			
+			driver=new FirefoxDriver();
+			
+		}
+		
+		else if(Browser.equals("IE")) {
+			
+			System.setProperty("webdriver.ie.driver", "./Drivers/IEDriverServer.exe");
+			
+			driver=new InternetExplorerDriver();
+			
+		}
+		
+		else {
+			System.out.println("Broser not initiated, please give correct browser name>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		}
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		
+		return driver;
+		
+	}
+	
 	public static void quitBrowser(WebDriver driver) {
 		
 		driver.quit();
